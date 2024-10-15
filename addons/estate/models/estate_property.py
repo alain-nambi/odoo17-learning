3446714b0fcc9b7d27127ac95b437239db83e60c
Alain Nambii <alainnambi@gmail.com>
2024-10-15 09:37:26 +0300

    models : Update estate property models * Set the selling price as read-only * Prevent copying of the availability date and the selling price values * The default number of bedrooms is 2 * The default availability date is in 3 months

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,17 +1,28 @@
 from odoo import fields, models
+from datetime import date, timedelta
 
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
+    def _default_date_availability(self):
+        return date.today() + timedelta(days=90)
+
     name = fields.Char(string='Property Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='PostCode')
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=_default_date_availability
+    )
     expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    selling_price = fields.Float(
+        string='Selling Price',
+        readonly=True,
+        copy=False
+    )
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
