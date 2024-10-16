3e026e0e7eee1ddadde610a4ddb245f8b26bb9ea
Alain Nambii <alainnambi@gmail.com>
2024-10-16 12:53:49 +0300

    feat : Add property offer :  property offer is an amount a potential buyer offers to the seller

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -16,8 +16,9 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
-        'views/estate_type_views.xml',
-        'views/estate_tag_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer.xml'
     ],
     'installable': True,
     'application': True,
